3bd4303f8116fad885726942749422b047317963
Suraj_KY <face.surajyadav@gmail.com>
2023-11-14 14:10:29 +0530

    ResultViewController Functionality Complete

---
+++ BMI-Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  secondViewController.swift
+//  BMI-Calculator
+//
+//  Created by Suraj KY on 06/11/23.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bmiLabel.text = bmiValue
+        
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+}
