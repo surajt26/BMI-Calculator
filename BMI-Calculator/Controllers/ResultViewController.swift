4216090732d73d41f3bc4455683d250c5337fd6b
Suraj_KY <face.surajyadav@gmail.com>
2023-11-19 16:05:58 +0530

    Update ResultViewController

--- BMI-Calculator/Controllers/ResultViewController.swift
+++ BMI-Calculator/Controllers/ResultViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ResultViewController: UIViewController {
     
     var bmiValue: String?
+    var bmiAdvice: String?
+    var bmiColor: UIColor?
     
     @IBOutlet weak var bmiLabel: UILabel!
     @IBOutlet weak var adviceLabel: UILabel!
@@ -18,12 +20,11 @@ class ResultViewController: UIViewController {
         super.viewDidLoad()
         
         bmiLabel.text = bmiValue
-        
+        adviceLabel.text = bmiAdvice
+        view.backgroundColor = bmiColor
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
-        
     }
-    
 }
